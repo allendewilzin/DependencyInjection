1ce46a169917e72e4aab52898bb8f11caa66f0bf
AJ <ajdewilzin@gmail.com>
2024-05-07 16:50:47 +0200

    Initial commit

---
+++ DependencyInjection/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public interface IUserRepository
+    {
+        void Save(User user);
+        void Edit(User user, bool editable);
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        public void Save(User user)
+        {
+            // Implementation to save user to the database
+            Console.WriteLine($"Saving user: {user.Name}");
+        }
+        public void Edit(User user, bool editable)
+        {
+            // Implementation to save user to the database
+            Console.WriteLine($"Saving user: {user.Name}");
+        }
+    }
+}
---
+++ DependencyInjection/UserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        // Constructor Injection
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void RegisterUser(string name)
+        {
+            var user = new User { Name = name };
+            _userRepository.Save(user);
+        }
+    }
+}
